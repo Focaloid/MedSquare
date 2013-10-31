0f870b63b184d3a6b58d67aef9c1ff1db5b6e69f
delbin thomas <delbincthomas@gmail.com>
2013-10-31 14:29:55 +0530

    third commit after adding gps coordinates

--- src/com/medsquare/medsquare/MainActivity.java
+++ src/com/medsquare/medsquare/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		
+		super.setIntegerProperty("splashscreen",R.drawable.splash);
+		
         super.loadUrl("file:///android_asset/www/index.html");
     }
 
